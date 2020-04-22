37d8ac3349f1921a4c75b63e30c0f8bcade60be2
o1lo01ol1o <tim.pierson@gmail.com>
2020-04-22 15:04:04 +0100

    Create the project

---
+++ app/Main.hs
@@ -0,0 +1,7 @@
+module Main (main) where
+
+import StreamlyVinyl (someFunc)
+
+
+main :: IO ()
+main = someFunc
---
+++ src/StreamlyVinyl.hs
@@ -0,0 +1,15 @@
+{- |
+Copyright: (c) 2020 o1lo01ol1o
+SPDX-License-Identifier: MIT
+Maintainer: o1lo01ol1o <repos@befunctional.io>
+
+utilities for dealing with vinyl records within streamly streams
+-}
+
+module StreamlyVinyl
+       ( someFunc
+       ) where
+
+
+someFunc :: IO ()
+someFunc = putStrLn ("someFunc" :: String)
